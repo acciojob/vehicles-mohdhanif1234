83c19324a0a7f17f122d3d4f21f3185b7a5b16f6
mohdhanif1234 <hanif.topia@gmail.com>
2022-12-17 15:06:13 +0530

    Submitted Vehicles Assignment

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,9 +3,15 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    @Override
     public String getVehicleName() {
         return this.name;
     }
+    @Override
     public int getVehicleCapacity() {
         return this.capacity;
     }
